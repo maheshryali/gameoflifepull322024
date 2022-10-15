64585cc729e272866689ca44a8a252cfe0ac7f09
mahesh <maheshryali@gmail.com>
2022-10-15 15:32:52 +0530

    changes1

---
+++ jenkinsparametersfile.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent { label 'GAMEOFLIFE' }
+    parameters {
+        choice(name:'Java_version', choices: ['8', '11'], description: 'forbranchselection')
+        choice(name:'branchname', choices: ['master', 'branch1'], description: 'forbranchselection')
+        choice(name:'MAVEN', choices: ['clean', 'package','clean install','clean package','test'], description: 'forbranchselection')
+        //string(name: 'MAVEN1', defaultValue: 'package', description: 'forselectingmaven')
+    }
+    stages {
+        stage('vcs') {
+            steps {
+            git branch: "${params.branchname}",
+                   url: 'https://github.com/maheshryali/game-of-life.git'
+                   }
+        }
+        stage('mavenbuild') {
+            steps {
+                export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH"
+                sh " mvn ${params.MAVEN}"
+                /*
+                script{
+                    if (${params.Java_version} == '8'){
+                        export JAVA_HOME=""
+                        sh " mvn ${params.MAVEN}"
+                    }
+                    elseif (${params.Java_version} == '11'){
+                        export JAVA_HOME=""
+                        sh " mvn ${params.MAVEN}"
+                    }
+                    else {
+                        sh " mvn ${params.MAVEN}"
+                    }
+                }
+                */
+            }
+        }
+    }
+}
